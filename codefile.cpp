be69847fbc742c7ba7dafdd853414281eac2e2ab
Ayush Pandey <ayushpandey2442@gmail.com>
2024-04-02 21:32:37 +0530

    Print single number

---
+++ codefile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Please enter a number: ";    
+    std::cin >> number;
+    std::cout << "The number you entered is: " << number << std::endl;
+
+    return 0;
+}
+
